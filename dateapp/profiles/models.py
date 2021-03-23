64760338c64aa9ca0bfedb9b0acda3617e520f72
Oleg <urban.oleg1@gmail.com>
2021-03-23 10:15:23 +0300

    Create models for profiles and their photos

--- dateapp/profiles/models.py
+++ dateapp/profiles/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Profile(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    country = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    date_of_birth = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['first_name']
+
+    def __str__(self):
+        return self.first_name
+
+
+class Photo(models.Model):
+    profile = models.ForeignKey(Profile, related_name='photos', on_delete=models.CASCADE)
+    url = models.URLField()
+    image = models.ImageField(upload_to='profiles_photo')
+    created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created']
