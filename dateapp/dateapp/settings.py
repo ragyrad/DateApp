8d176c672a1a972895d909672cff6c408ef5bf85
Oleg <urban.oleg1@gmail.com>
2021-03-23 10:08:41 +0300

    Create app for user profiles

--- dateapp/dateapp/settings.py
+++ dateapp/dateapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles.apps.ProfilesConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles.Profile'
