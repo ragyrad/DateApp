e19fd4369f4ece105e1c5ff8127b7b3487589094
Oleg <urban.oleg1@gmail.com>
2021-03-23 10:19:50 +0300

    Register Profile model on the admin site

--- dateapp/profiles/admin.py
+++ dateapp/profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'slug', 'phone', 'country', 'city', 'date_of_birth')
+    prepopulated_fieldsa = {'slug': ('id',)}
